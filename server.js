17bcf7765412d6cddd5985fd06f3795f28524518
Herman Ng <heymanhn@gmail.com>
2016-08-30 17:57:57 -0700

    Add semicolons back to server.js

--- server.js
+++ server.js
@@ -4,27 +4,27 @@
  * Simple web server for Journey web frontend
  */
 
-'use strict'
-let express = require('express')
-let fs = require('fs')
-let https = require('https')
-let path = require('path')
+'use strict';
+let express = require('express');
+let fs = require('fs');
+let https = require('https');
+let path = require('path');
 
-let app = express()
-app.set('port', (process.env.PORT || 3010))
-app.use(express.static('app'))
+let app = express();
+app.set('port', (process.env.PORT || 3010));
+app.use(express.static('app'));
 
 // send all requests to index.html so browserHistory in React Router works
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'app', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, 'app', 'index.html'));
+});
 
 const options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
-}
+};
 
-app.server = https.createServer(options, app)
+app.server = https.createServer(options, app);
 app.server.listen(app.get('port'), () => {
-  console.log('Server started: https://localhost:' + app.get('port') + '/')
-})
+  console.log('Server started: https://localhost:' + app.get('port') + '/');
+});
