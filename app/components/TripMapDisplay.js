cdd978e6efffc5fd193482eead3db41ca2802da1
Herman Ng <heymanhn@gmail.com>
2016-09-26 23:36:51 -0700

    Fit map to a trip destination's viewport
    When the trip page loads, the map will be bound to the trip
    destination's viewport. [#130792677]

--- app/components/TripMapDisplay.js
+++ app/components/TripMapDisplay.js
@@ -6,14 +6,24 @@ import { mapbox } from '../constants';
 
 class TripMapDisplay extends Component {
   componentDidMount() {
+    // Initialize the map and set it to the viewport of the destination
     const { destination } = this.props;
     mapboxgl.accessToken = mapbox.token;
 
     const map = new mapboxgl.Map({
       container: this.container,
-      style: 'mapbox://styles/mapbox/streets-v9',
+      style: mapbox.streetsStyle,
+      attributionControl: true,
       center: destination.loc.coordinates,
-      zoom: 9
+    });
+
+    const { southwest, northeast } = destination.viewport;
+    const bounds = new mapboxgl.LngLatBounds(
+      southwest.coordinates,
+      northeast.coordinates
+    );
+    map.fitBounds(bounds, {
+      linear: true
     });
   }
 
