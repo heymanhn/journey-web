4294db3fbcc721a3faabc18d3ef9e2534f94a5f3
Herman Ng <heymanhn@gmail.com>
2016-10-06 15:58:11 -0700

    Don't redirect to /trips when displaying Trips on homepage
    Previously, we would redirect, but that causes an extra entry to be
    added to the browser history. When the user clicks Back, they will be
    redirected back to the current page. [Finishes #131848599]

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -4,19 +4,13 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 import Intro from './Intro';
+import Trips from './Trips';
 import { viewTripsPage } from 'app/actions/navigation';
 
 class Home extends Component {
-  componentWillMount() {
-    const { authState } = this.props;
-    if (authState.token) {
-      this.props.onViewTripsPage();
-    }
-  }
-
   render() {
     if (this.props.authState.token) {
-      return null;
+      return <Trips />;
     }
 
     return <Intro />;
@@ -24,7 +18,7 @@ class Home extends Component {
 }
 
 Home.propTypes = {
-  authState: PropTypes.object,
+  authState: PropTypes.object.isRequired,
   onViewTripsPage: PropTypes.func.isRequired
 };
 
