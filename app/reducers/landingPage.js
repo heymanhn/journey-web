d21b3d4c7fdbdb7f118f55e655af29a6813f90f2
Herman Ng <heymanhn@gmail.com>
2016-10-26 19:12:29 -0700

    Reset landing page state only upon logout
    This change prevents the signup and login page analytics events to be
    fired erroneously. [Finishes #133219321]

--- app/reducers/landingPage.js
+++ app/reducers/landingPage.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { API_LOGIN_SUCCESS, API_SIGNUP_SUCCESS } from 'app/actions/auth';
+import { LOGOUT } from 'app/actions/auth';
 import {
   SET_PAGE_LOGIN_STATE,
   SET_PAGE_SIGNUP_STATE,
@@ -18,8 +18,7 @@ export default function landingPageState(state = initialLPState, action) {
     case SET_OVERRIDE_FRAME:
       return { ...state, overrideFrame: action.frame };
     case CLEAR_OVERRIDE_FRAME:
-    case API_LOGIN_SUCCESS:
-    case API_SIGNUP_SUCCESS:
+    case LOGOUT:
       return initialLPState;
   }
 
