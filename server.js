9e2fa741fdf7327b095c50bccd51b5ecbbf24777
Herman Ng <heymanhn@gmail.com>
2016-10-28 22:53:34 -0700

    Ensure all API calls use HTTPS
    [Finishes #131610731]

--- server.js
+++ server.js
@@ -12,22 +12,37 @@ let http = require('http');
 let path = require('path');
 
 let app = express();
+const env = app.get('env');
 app.set('port', (process.env.PORT || 3010));
 app.use(express.static('app'));
+
+app.enable('trust proxy');
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+
+  return res.redirect(`https://${req.hostname}${req.url}`);
+});
 
 // send all requests to index.html so browserHistory in React Router works
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'app', 'index.html'));
 });
 
-// Disable for now since we will be running on HTTP
-// const options = {
-//   key: fs.readFileSync('key.pem'),
-//   cert: fs.readFileSync('cert.pem')
-// };
-// app.server = https.createServer(options, app);
+// Set up the HTTPS server if on dev
+if (env === 'development') {
+  const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+  };
 
-app.server = http.createServer(app);
+  app.server = https.createServer(options, app);
+} else {
+  app.server = http.createServer(app);
+}
+
 app.server.listen(app.get('port'), () => {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
+  const protocol = env === 'development' ? 'https' : 'http';
+  console.log(`${protocol} server started at port ${app.get('port')}`);
 });
